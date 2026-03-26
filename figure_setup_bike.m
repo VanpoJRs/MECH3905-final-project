f4bf23223229f399bde58c92a9e95aea02759d6b
Jerry Du <vanpojrs52@gmail.com>
2026-03-26 08:56:17 -0300

    Add files via upload

---
+++ figure_setup_bike.m
@@ -0,0 +1,53 @@
+
+%% function to setup figure window 
+function [j,ball_image,alpha_channel]=figure_setup_bike()
+% adjust figure to desired size&position, then use f.Position to get #s
+j=figure('position',[0 50 3840 2160]); %%width then height form bottom left 
+hold on % prevent axes from flipping y-axis when plotting image
+
+%axis set up 
+% axis are equal in size
+% to use up full figure window resize and reposition axes of plot
+% to its normalized limits (0 to 1)
+axis('equal') % set aspect ratio so equal tick mark increments on each 
+axis('manual') % freeze all axis limits for subsequent plots so they
+% do not automatically adjust on the fly
+
+ax=gca; % get current axes handle
+ax.Position=[0 0 1 1]; % normalized position of axes [left bottom width height]
+set(gcf,'Toolbar','none','Menu','none'); % remove toolbar and menu
+set(gca,'visible','off'); % remove axis labels
+set(gcf,'color','w'); % make figure background white
+set(gca, 'Position', [0 0 1 1])
+
+ylim([0 2160]) % set y axis limits
+xlim([-100 3840]) %set x axis limits 
+
+%% background picture load in 
+bg=imread('newbkg_v1.png'); 
+bg=flipud(bg); %flip so right side up. Because background image, dont need alpha.
+
+%bk1= image(bg); %bk is now handle to modify later if needed (stores memory
+%abt this background picture). 
+
+%Want to set the x and y lim of picture to
+%match figure window.
+image(bg, ...
+    'Xdata', [-90 3840],...
+    'YData', [0 2160]); 
+
+
+hold on %so figures can lay overtop
+
+
+%% Insert character player here **
+% read image of black ball to track
+% for fast performance, make image using as few pixels as possible
+[ball_image,~,alpha_channel]=imread('circle_black_transparent.png');
+ball_image=flipud(ball_image); % need to flip image so it is oriented correctly
+% images are stored so that they face upwards when
+% y axes are reversed (positive downwards),
+% but our y axis is normal (postive upwards)
+% so we need to flip the image so it faces upward
+alpha_channel=flipud(alpha_channel);
+end
