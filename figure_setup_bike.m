47df4106d0f24958746a24694cb7cb092e2d4e5d
Pei-place <elizferd24@gmail.com>
2026-03-31 15:25:01 -0300

    Update figure_setup_bike.m

--- figure_setup_bike.m
+++ figure_setup_bike.m
@@ -1,8 +1,9 @@
 
 %% function to setup figure window 
 function [j,ball_image,alpha_channel]=figure_setup_bike()
+
 % adjust figure to desired size&position, then use f.Position to get #s
-j=figure('position',[0 50 3840 2160]); %%width then height form bottom left 
+j=figure('position',[0 50 3840 2160]); %%width then height form bottom left. Size in pixels 
 hold on % prevent axes from flipping y-axis when plotting image
 
 %axis set up 
@@ -33,14 +34,54 @@ bg=flipud(bg); %flip so right side up. Because background image, dont need alpha
 %Want to set the x and y lim of picture to
 %match figure window.
 image(bg, ...
-    'Xdata', [-90 3840],...
+    'Xdata', [-90 4000],...
     'YData', [0 2160]); 
 
 
 hold on %so figures can lay overtop
+impixelinfo
+
+%% static obstacle oil 
+%obstacle 3 call; oil puddle 
+scale_oil= 1.05; 
+[oil_big,~, oil_alphabig]= imread('oil_puddle_v2.png'); 
+oil_big=flipud(oil_big); 
+oil_alphabig=flipud(oil_alphabig); 
+oil= imresize(oil_big, scale_oil);
+oil_alpha=imresize(oil_alphabig, scale_oil); 
+[b,a,~]= size(oil); 
+hoil=image(oil); %render oil image
+
+set(hoil,...
+    'Xdata', [2000-a/2, 2000+a/2], ...
+    'Ydata', [1955-b/2, 1955+b/2], ...
+    'AlphaData', oil_alpha); 
+
+maskoil= oil_alpha> 0; %creating bool mask for oil 
+
+
+scale_pothole= 0.6;
+
+[pothole_big,~, pothole_alpha_big] = imread('pot_hole.png'); 
+pothole_big=flipud(pothole_big);
+pothole_alpha_big= flipud(pothole_alpha_big); 
+
+pothole= imresize(pothole_big, scale_pothole); 
+pothole_alpha= imresize(pothole_alpha_big, scale_pothole); 
+[b1,a1,~]= size(pothole); 
+hpothole = image(pothole); %render image 
+
+
+set(hpothole, ...
+    'Xdata', [3600-a1/2, 3600+a1/2], ...
+    'Ydata', [400-b1/2, 400+b1/2], ...
+    'AlphaData', pothole_alpha);  
+
+
 
 
 %% Insert character player here **
+
 % read image of black ball to track
 % for fast performance, make image using as few pixels as possible
 [ball_image,~,alpha_channel]=imread('circle_black_transparent.png');
