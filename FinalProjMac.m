dc9e07b60bbc17a0eb8304874d1ec00bb49eff1a
Jerry Du <vanpojrs52@gmail.com>
2026-04-01 17:15:46 -0300

    Update FinalProjMac.m

--- FinalProjMac.m
+++ FinalProjMac.m
@@ -37,8 +37,8 @@ end
 mask = double(mask);
 %============================================================================
 
-global m c ux uy startscreen % declare global variables used in ODE
-global gamestate
+global m c ux uy% declare global variables used in ODE
+global gamestate x_next x_predict x_current
 gamestate = "wait";
 
 startscreen='start';
